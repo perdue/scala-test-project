cba51a8b4e6798efc0e5563a5d0394318ce03c69
Brent Perdue <perdue@ccri.com>
2013-10-28 11:08:34 -0400

    Importing files to repo.

---
+++ src/main/scala/org/perdue/App.scala
@@ -0,0 +1,12 @@
+package org.perdue
+
+/**
+ * Hello world!
+ *
+ */
+object App 
+{
+    def main(args: Array[String]):Unit={
+        println( "Hello World!" )
+    }
+}
---
+++ src/test/scala/org/perdue/AppTest.scala
@@ -0,0 +1,17 @@
+package org.perdue
+
+import org.junit.Test
+import org.junit.Assert._
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    def testApp():Unit={
+        assertTrue( true )
+    }
+}
