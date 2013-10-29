cd1ead2aa60a43e3060d22fd7b1cdfe13e6e20ae
Brent Perdue <perdue@ccri.com>
2013-10-29 09:40:40 -0400

    Modified file to try out another object.

--- src/main/scala/org/perdue/App.scala
+++ src/main/scala/org/perdue/App.scala
@@ -1,12 +1,24 @@
 package org.perdue
 
-/**
- * Hello world!
- *
- */
-object App 
-{
-    def main(args: Array[String]):Unit={
-        println( "Hello World!" )
+//object App 
+//{
+//    def main(args: Array[String]):Unit={
+//        println( "Hello World!" )
+//    }
+
+//    def main(args: Array[String]) {
+//        println( "Hello World!" )
+//    }
+//}
+
+import java.util.{Date, Locale}
+import java.text.DateFormat
+import java.text.DateFormat._
+
+object FrenchDate {
+    def main(args: Array[String]) {
+        val now = new Date
+        val df = getDateInstance(LONG, Locale.FRANCE)
+        println(df format now)
     }
 }
